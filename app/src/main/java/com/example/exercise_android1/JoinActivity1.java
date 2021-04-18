f1b2245395ec200b898572f628d9a8fb80dd5dfd
MinJi <jeongminji4490>
2021-04-18 18:58:24 +0900

    JoinActivity1&JoinActivity2 수정, db에 들어갈 회원정보 string 형태로 변환했습니당 오류나면 얘기해주세욥 ㅠㅠㅠ

--- app/src/main/java/com/example/exercise_android1/JoinActivity1.java
+++ app/src/main/java/com/example/exercise_android1/JoinActivity1.java
@@ -6,11 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class JoinActivity1 extends AppCompatActivity {
 
     ImageButton joinNextBtn;
+    /*아이디,비밀번호,비밀번호확인,이름,폰번호에 대한 Edittext(activity_join1에서 확인 가능)*/
+    EditText idEdit,pwEdit,pwOkEdit,nameEdit,phoneEdit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,11 +21,23 @@ public class JoinActivity1 extends AppCompatActivity {
         setContentView(R.layout.activity_join1);
 
         joinNextBtn=(ImageButton)findViewById(R.id.joinNextBtn); /*회원가입 다음단계로 이동*/
+        idEdit=(EditText)findViewById(R.id.joinIdEdit);
+        pwEdit=(EditText)findViewById(R.id.joinPassEdit1);
+        pwOkEdit=(EditText)findViewById(R.id.joinPassEdit2);
+        nameEdit=(EditText)findViewById(R.id.joinNameEdit);
+        phoneEdit=(EditText)findViewById(R.id.joinPhoneEdit);
 
         /*버튼 수가 적어서 switch문 안쓰고 setOnClickListener 사용*/
+        /*joinNextBtn(회원가입 다음 단계로 이동)버튼 누르면 db로 위 editText로부터 입력된 string 데이터들을 저장*/
         joinNextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*editText로부터 입력받은 각 항목들을 string형태로 변환*/
+                String id=idEdit.getText().toString().trim();
+                String pw=pwEdit.getText().toString().trim();
+                String pwOk=pwOkEdit.getText().toString().trim();
+                String name=nameEdit.getText().toString().trim();
+                String phone=phoneEdit.getText().toString().trim();
                 Intent intent=new Intent(JoinActivity1.this,JoinActivity2.class);
                 startActivity(intent);
 
--- app/src/main/java/com/example/exercise_android1/JoinActivity2.java
+++ app/src/main/java/com/example/exercise_android1/JoinActivity2.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 public class JoinActivity2 extends AppCompatActivity {
 
     ImageButton joinOkBtn; /*회원가입 완료 버튼*/
+    EditText cmEdit,kgEdit; /*키와 체중을 입력받을 EditText(activity_join2)*/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +19,8 @@ public class JoinActivity2 extends AppCompatActivity {
         setContentView(R.layout.activity_join2);
 
         joinOkBtn=(ImageButton)findViewById(R.id.JoinOkBtn);
+        cmEdit=(EditText)findViewById(R.id.cmEditText);
+        kgEdit=(EditText)findViewById(R.id.kgEditText);
 
         /*jsp와 연동하여 db에 회원정보 저장하는 알고리즘 필요*/
         /*회원가입 성공 토스트메세지 필요*/
@@ -24,6 +28,8 @@ public class JoinActivity2 extends AppCompatActivity {
         joinOkBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String cm=cmEdit.getText().toString().trim();
+                String kg=kgEdit.getText().toString().trim();
                 Intent intent=new Intent(JoinActivity2.this,MainActivity2.class);
                 startActivity(intent);
             }
