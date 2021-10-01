14f080022e4278c1d3f1b9558eaebdb8056963a5
ji-moon park <wocky015@gmail.com>
2021-10-01 11:35:04 +0900

    graph패키지 업데이트
    1. BMI 파트 추가
     1) 레이아웃 graph_bmi_dialog.xml 추가
     2) 라이브러리 추가 및 graph패키지 관련 파일 수정
    
    2. 지난 28일에 말씀드린 사항은 라이브러리 관련 오류에 막혀서 아직 원인 파악중에 있습니다..

--- app/src/main/java/com/example/exercise_android1/graph/graph.java
+++ app/src/main/java/com/example/exercise_android1/graph/graph.java
@@ -9,21 +9,28 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.exercise_android1.R;
+import com.github.mikephil.charting.charts.HorizontalBarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.jjoe64.graphview.GraphView;
 import com.jjoe64.graphview.DefaultLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -31,26 +38,31 @@ public class graph extends AppCompatActivity {
 
     public CalendarView calendarView;
 
-    String value;
+    String value, bmi_info;
     GraphView graphView;
     LineGraphSeries<DataPoint> series;
 
     SimpleDateFormat sdf = new SimpleDateFormat("MM.dd");
-
     Button button;
 
     Date date;
     long day;
-    float f;
+    float f, cm, kg, bmi;
 
     graphDBHelper graphdbHelper;
     SQLiteDatabase db;
     Cursor temp;
 
+    HorizontalBarChart bar_chart;
+    int[] colorArray = new int[]{Color.GRAY, Color.CYAN, Color.GREEN, Color.YELLOW, Color.RED};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.graph_main);
+
+        TextView textview = (TextView) findViewById(R.id.bmi);
+        TextView textView2 = (TextView) findViewById(R.id.bmi_result);
 
         graphdbHelper = new graphDBHelper(graph.this);
 
@@ -59,14 +71,90 @@ public class graph extends AppCompatActivity {
 
         series = new LineGraphSeries<DataPoint>();
         graphView.addSeries(series);
-        graphView.getGridLabelRenderer().setNumHorizontalLabels(5);
-
 
         db = graphdbHelper.getWritableDatabase();
 
         button = (Button) findViewById(R.id.button);
 
         calendarView = findViewById(R.id.calendarView);
+
+        bar_chart = (HorizontalBarChart) findViewById(R.id.bar_chart);
+        BarDataSet barDataSet = new BarDataSet(dataValues(), "");
+        barDataSet.setColors(colorArray);
+        barDataSet.setStackLabels(new String[]{"저체중", "정상", "과체중", "비만", "고도비만"});
+
+        BarData barData = new BarData(barDataSet);
+        barData.setBarWidth(0.8f);
+        barData.setValueTextSize(10);
+        barData.setDrawValues(false);
+
+        bar_chart.setData(barData);
+        bar_chart.setDescription(null);
+
+        bar_chart.setTouchEnabled(false);
+        bar_chart.getXAxis().setEnabled(false);
+        bar_chart.getXAxis().setLabelCount(0, false);
+        bar_chart.getXAxis().setDrawAxisLine(false);
+        bar_chart.getXAxis().setDrawGridLines(false);
+        bar_chart.getXAxis().setDrawLabels(false);
+        bar_chart.getAxisRight().setDrawGridLines(false);
+        bar_chart.getAxisLeft().setDrawAxisLine(false);
+        bar_chart.getAxisLeft().setDrawLabels(false);
+        bar_chart.getAxisLeft().setDrawGridLines(false);
+
+        bar_chart.getAxis(null).setDrawAxisLine(false);
+        bar_chart.getAxis(null).setDrawLabels(false);
+
+        /** BMI 계산을 위한 신체 데이터 입력 **/
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(graph.this);
+                LayoutInflater inflater = getLayoutInflater();
+                View view = inflater.inflate(R.layout.graph_bmi_dialog, null);
+                builder.setView(view);
+                final Button submit = (Button) view.findViewById(R.id.buttonSubmit);
+                final EditText height = (EditText) view.findViewById(R.id.edittextheight);
+                final EditText weight = (EditText) view.findViewById(R.id.edittextweight);
+
+                final AlertDialog dialog = builder.create();
+
+                submit.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View v) {
+                        String _height = height.getText().toString();
+                        String _weight = weight.getText().toString();
+
+                        cm = Float.parseFloat(_height);
+                        kg = Float.parseFloat(_weight);
+                        graphdbHelper.bmi_updateData(db, cm, kg);
+
+                        cm = cm / 100;
+                        bmi = kg / (cm * cm);
+                        bmi_info = String.format("BMI = %.1f", bmi);
+                        textview.setText(bmi_info);
+
+                        if(bmi >= 30){
+                            textView2.setText("고도비만");
+                        }
+                        else if(bmi < 30 && bmi >= 25){
+                            textView2.setText("비만");
+                        }
+                        else if(bmi < 25 && bmi >= 23){
+                            textView2.setText("과체중");
+                        }
+                        else if(bmi < 23 && bmi >= 18.5){
+                            textView2.setText("정상");
+                        }
+                        else{
+                            textView2.setText("저체중");
+                        }
+                        dialog.dismiss();
+                    }
+                });
+
+                dialog.show();
+            }
+        });
 
         /** 달력에서 날짜 선택하여 데이터를 입력하고 그래프에 추가 **/
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
@@ -115,8 +203,13 @@ public class graph extends AppCompatActivity {
                         //x축에 표시되는 수치를 DB에 저장된 데이터수만큼 표시
                         temp = db.query("DP_table", null, null, null, null, null, null);
                         graphView.getGridLabelRenderer().setNumHorizontalLabels(temp.getCount());
-                        graphView.getViewport().setScrollable(true);
+                        graphView.getViewport().setXAxisBoundsManual(true);
+                        graphView.getViewport().setYAxisBoundsManual(true);
+                        graphView.getViewport().setMinY(30);
+                        graphView.getViewport().setScalable(true);  // activate horizontal zooming and scrolling
+                        graphView.getViewport().setScrollable(true);  // activate horizontal scrolling
                         graphView.getGridLabelRenderer().setHumanRounding(false);
+
                     }
                 });
 
@@ -129,8 +222,38 @@ public class graph extends AppCompatActivity {
             }
         });
 
+
+        /** 앱실행시 DB에 존재하는 데이터를 참조하여 BMI 정보 입력 **/
+        Cursor res = db.rawQuery("select * from bmi_table", null);
+        if(res.getCount() != 0){
+            res.moveToNext();
+            cm = res.getFloat(1);
+            kg = res.getFloat(2);
+            cm = cm / 100;
+            bmi = kg / (cm * cm);
+            bmi_info = String.format("BMI = %.1f", bmi);
+            textview.setText(bmi_info);
+
+            if(bmi >= 30){
+                textView2.setText("고도비만");
+            }
+            else if(bmi < 30 && bmi >= 25){
+                textView2.setText("비만");
+            }
+            else if(bmi < 25 && bmi >= 23){
+                textView2.setText("과체중");
+            }
+            else if(bmi < 23 && bmi >= 18.5){
+                textView2.setText("정상");
+            }
+            else{
+                textView2.setText("저체중");
+            }
+        }
+
+
         /** 앱실행시 DB에 존재하는 데이터를 참조하여 그래프에 데이터추가 **/
-        Cursor res = graphdbHelper.getAllDate();
+        res = graphdbHelper.getAllDate();
         if(res.getCount() == 0){
             graphView.addSeries(series);
         }
@@ -141,9 +264,18 @@ public class graph extends AppCompatActivity {
             series.setThickness(4); //그래프 선 두께
             series.setDrawBackground(true); //그래프 배경색 유무
             series.setDrawDataPoints(true); //그래프 데이터 점표시 유무
+            temp = db.query("DP_table", null, null, null, null, null, null);
+            graphView.getGridLabelRenderer().setNumHorizontalLabels(temp.getCount());
+//            graphView.getGridLabelRenderer().setNumHorizontalLabels(5);
+            graphView.setHorizontalScrollBarEnabled(true);
+            graphView.getViewport().setScrollable(true);  // activate horizontal scrolling
+            graphView.getViewport().setScrollableY(true);
+            graphView.getViewport().setXAxisBoundsManual(true);
+            graphView.getViewport().setYAxisBoundsManual(true);
+            graphView.getViewport().setMinY(30);
+            graphView.getGridLabelRenderer().setHumanRounding(false);
 
         }
-        graphView.getViewport().setScrollable(true);
 
         /** 그래프 x축의 날짜 데이터 포멧변환 **/
         graphView.getGridLabelRenderer().setLabelFormatter(new DefaultLabelFormatter() {
@@ -156,10 +288,6 @@ public class graph extends AppCompatActivity {
                 }
             }
         });
-        temp = db.query("DP_table", null, null, null, null, null, null);
-        graphView.getGridLabelRenderer().setNumHorizontalLabels(temp.getCount());
-        graphView.getViewport().setScrollable(true);
-        graphView.getGridLabelRenderer().setHumanRounding(false);
     }
 
     /** 내부 DB에 저장된 데이터를 날짜순으로 읽고 저장하여 그래프에 대입하기 위한 메소드 **/
@@ -179,5 +307,13 @@ public class graph extends AppCompatActivity {
             dataPoints[i] = new DataPoint(date, f);
         }
         return dataPoints;
+    }
+
+    private ArrayList dataValues(){
+        ArrayList dataVals = new ArrayList<>();
+
+        dataVals.add(new BarEntry(0, new float[]{18.5f, 23, 25, 30, 43}));
+
+        return dataVals;
     }
 }
